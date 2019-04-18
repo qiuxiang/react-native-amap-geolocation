66ba97f61a41f9dba4914efa330aa6515782f525
7c00 <i@7c00.cc>
2019-04-18 22:51:09 +0800

    完善 ios 接口

--- lib/ios/AMapGeolocation.m
+++ lib/ios/AMapGeolocation.m
@@ -32,6 +32,34 @@ RCT_EXPORT_METHOD(setLocatingWithReGeocode : (BOOL)value) {
   [_manager setLocatingWithReGeocode:value];
 }
 
+RCT_EXPORT_METHOD(setAllowsBackgroundLocationUpdates : (BOOL)value) {
+  [_manager setAllowsBackgroundLocationUpdates:value];
+}
+
+RCT_EXPORT_METHOD(setPausesLocationUpdatesAutomatically : (BOOL)value) {
+  [_manager setPausesLocationUpdatesAutomatically:value];
+}
+
+RCT_EXPORT_METHOD(setDesiredAccuracy : (int)value) {
+  [_manager setDesiredAccuracy:value];
+}
+
+RCT_EXPORT_METHOD(setDistanceFilter : (int)value) {
+  [_manager setDistanceFilter:value];
+}
+
+RCT_EXPORT_METHOD(setGeoLanguage : (int)value) {
+  [_manager setReGeocodeLanguage:(AMapLocationReGeocodeLanguage)value];
+}
+
+RCT_EXPORT_METHOD(setReGeocodeTimeout : (int)value) {
+  [_manager setReGeocodeTimeout:value];
+}
+
+RCT_EXPORT_METHOD(setLocationTimeout : (int)value) {
+  [_manager setLocationTimeout:value];
+}
+
 - (id)json:(CLLocation *)location reGeocode:(AMapLocationReGeocode *)reGeocode {
   if (reGeocode && reGeocode.formattedAddress.length) {
     return @{
