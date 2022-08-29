33885fec3e945cb5847419f6664fe0b3cd97ac0f
7c00 <i@7c00.cc>
2022-08-29 16:26:33 +0800

    Update ios project

--- ios/exampleTests/exampleTests.m
+++ ios/exampleTests/exampleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNAMapGeolocationTests : XCTestCase
+@interface exampleTests : XCTestCase
 
 @end
 
-@implementation RNAMapGeolocationTests
+@implementation exampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
